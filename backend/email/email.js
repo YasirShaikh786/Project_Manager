ea9e0e8d38242a818f0ceeaed0a46ce93c0d781a
Yasir__Shaikh <yasirshaikhpune@gmail.com>
2025-03-20 20:38:40 +0530

    refresh token

--- backend/email/email.js
+++ backend/email/email.js
@@ -9,10 +9,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const CLIENT_ID = process.env.VITE_CLIENT_ID;
-const CLIENT_SECRET = process.env.VITE_CLIENT_SECRET;
-const REDIRECT_URI = process.env.VITE_REDIRECT_URI;
-const REFRESH_TOKEN = process.env.VITE_REFRESH_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
