a24109b68d18f1fed8f21dd18c2987b0468cad01
Yasir__Shaikh <yasirshaikhpune@gmail.com>
2025-04-12 20:40:35 +0530

    Adjuting to previous commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,9 +3,6 @@ import {
 	login,
 	logout,
 	signup,
-	verifyEmail,
-	forgotPassword,
-	resetPassword,
 	checkAuth,
 } from "../controllers/authController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
@@ -15,7 +12,7 @@ const router = express.Router();
 router.get("/check-auth", verifyToken, checkAuth);
 
 router.post("/signup", signup);
-router.post("/login", login);
+router.post("/login", login)	
 router.post("/logout", logout);
 
 // router.post("/verify-email", verifyEmail);
